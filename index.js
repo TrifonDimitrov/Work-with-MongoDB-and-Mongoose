ae295523eb5f787e4331e46e9ead68c4f196f2d2
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-24 15:20:28 +0200

    Database calling

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const {mongoClient, MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 const conectionString = `mongodb://localhost:27017`;
 const client = new MongoClient(conectionString);
+
+async function run() {
+    const db = client.db('admin');
+    const collection = db.collection('students');
+
+    const students = await collection.find().toArray();
+
+    console.log(students);
+}
+
+run();
