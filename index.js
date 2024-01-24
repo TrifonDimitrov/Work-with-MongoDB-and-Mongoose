033e2645f007b59ccde58dc262dda9d931acb7ed
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-24 16:01:41 +0200

    Create Mongoose schema

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(`mongodb://localhost:27017/admin`)
 .then(() => console.log('DB connected successfuly'));
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+
+});
